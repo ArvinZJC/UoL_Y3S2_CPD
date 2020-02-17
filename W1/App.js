8be887c6aa372aeb97a10aa68e6e7a3220fe1d05
ArvinZJC <zjcarvin@outlook.com>
2020-02-17 19:41:37 +0000

    Improve code format.

--- W1/App.js
+++ W1/App.js
@@ -1,19 +1,20 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
 // notice these are in the components folder
 import HomeActivity from './components/HomeActivity';
 import ProfileActivity from './components/ProfileActivity';
 
 // this is a navigator which tells the app that there are 2 screens/pages
 const RootStack = createStackNavigator(
-  {
-    Home: { screen: HomeActivity },
-    Profile: { screen: ProfileActivity },
-  },
-  {
-    // note we set the initial page to Home
-    initialRouteName: 'Home',
-  }
+    {
+        Home: {screen: HomeActivity},
+        Profile: {screen: ProfileActivity},
+    },
+    {
+        // note we set the initial page to Home
+        initialRouteName: 'Home',
+    }
 );
 
 const AppContainer = createAppContainer(RootStack);
--- W1/components/HomeActivity.js
+++ W1/components/HomeActivity.js
@@ -1,45 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 class HomeActivity extends React.Component
 {
-  // this creates a header with blue background
-  static navigationOptions = {
-    title: 'Home',
-    headerStyle: {
-      backgroundColor: '#03A9F4',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-  };
+    // this creates a header with blue background
+    static navigationOptions =
+    {
+        title: 'Home',
+        headerStyle:
+        {
+        backgroundColor: '#03A9F4',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle:
+        {
+        fontWeight: 'bold',
+        },
+    };
 
-  render()
-  {
-    return (
-      <View style = { styles.container }>
-        <Text style = { styles.headerText }>Home Activity</Text>
-        <Button title = 'Go to Profile Activity' onPress = { () => this.props.navigation.navigate('Profile') } />
-      </View>
-    );
-  }
+    render()
+    {
+        return(
+            <View style={styles.container}>
+                <Text style={styles.headerText}>Home Activity</Text>
+                <Button title='Go to Profile Activity' onPress={() => this.props.navigation.navigate('Profile')} />
+            </View>
+        );
+    }
 }
 
 // these are styles they are applied in the <View style = { styles.container }>
-const styles = StyleSheet.create( {
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    flex: 1,
-    justifyContent: 'center',
-  },
-  headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 10,
-    textAlign: 'center',
-  },
+const styles = StyleSheet.create(
+{
+    container:
+    {
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        flex: 1,
+        justifyContent: 'center',
+    },
+    headerText:
+    {
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 10,
+        textAlign: 'center',
+    },
 });
 
 // note this is the command that gives other pages access to it
